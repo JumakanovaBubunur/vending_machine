a1b2b0344961e57b27461b432e986c69058a11e8
Bubunur Jumakanova <jumakanovabibinur@gmail.com>
2023-12-08 18:36:27 +0600

    updated file

--- src/model/BankCardAcceptor.java
+++ src/model/BankCardAcceptor.java
@@ -3,72 +3,54 @@ package model;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class BankCardAcceptor {
+public class BankCardAcceptor extends VendingMachine {
     private static Scanner sc = new Scanner(System.in);
     private int cardNumber;
     private int code;
 
-    public int getSumm() {
-        return summ;
+    public BankCardAcceptor(int amount) {
+        super(amount);
+
     }
 
-    public void setSumm(int summ) {
-        this.summ = summ;
-    }
-
-    private int summ =1000;
-
-
-
+    @Override
     public void setData(){
         cardNumber = setCardNum();
         code = setCode();
     }
 
-    public static int setCardNum(){
-
-        System.out.println("Введите номер карты: ");
+    public static int setCardNum() {
+        System.out.println("Введите десятизначный номер карты: ");
         int number = 0;
         try {
-           number = sc.nextInt();
-           if (number<0){
-               System.out.println("Неверный ввод данных!");
-               setCardNum();
-           }
-           var length = String.valueOf(number).length();
-           if (length <16){
-               System.out.println("Неверная длина номера карты! Повторите ввод");
-               setCardNum();
-           }
-
-
-        }catch (InputMismatchException e){
+            number = sc.nextInt();
+            if (number < 0 || String.valueOf(number).length() < 8) {
+                System.out.println("Неверный ввод данных!");
+                number = setCardNum();
+            }
+        } catch (InputMismatchException | NumberFormatException e) {
             System.out.println("Неверный ввод данных!");
-            setCardNum();
+            sc.nextLine();
+            number = setCardNum();
         }
-
         return number;
     }
 
     public static int setCode() {
-        int code = 0;
         System.out.println("Введите четырёхзначный одноразовый код: ");
+        int code = 0;
         try {
             code = sc.nextInt();
-            if (code<0){
+            if (code < 0 || String.valueOf(code).length() != 4) {
                 System.out.println("Неверный ввод данных!");
-                setCode();
+                code = setCode();
             }
-            var length = String.valueOf(code).length();
-            if (length <4){
-                System.out.println("Неверная длина кода");
-                setCode();
-            }
-
-        }
-        catch (InputMismatchException e){
-            System.out.println("Неверный формат данных");
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.println("Неверный ввод данных!");
+            sc.nextLine();
+            code = setCode();
         }
         return code;
     }
+
 }
--- src/model/VendingMachine.java
+++ src/model/VendingMachine.java
@@ -1,4 +1,21 @@
 package model;
 
 public class VendingMachine {
+
+    public VendingMachine(int amount) {
+        this.amount = amount;
+    }
+
+    public void setData(){
+
+    }
+    private int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
